a7724dcc1163949ba4ef0b3275db3211d637d0e5
Porto <jgrporto26@gmail.com>
2025-10-02 01:04:28 -0300

    Adicionado logo

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -39,7 +39,7 @@ export default function LoginPage() {
           <img
             src="/footer-logo.png"
             alt="EMEX Play Logo"
-            className="w-[200px] h-auto"
+            className="w-[180px] h-auto"
           />
         </div>
         
